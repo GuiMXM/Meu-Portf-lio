7028f919affd21b613acebdff84a0db7dc020ef8
Guimxm <gui2918@gmail.com>
2022-11-21 15:27:16 -0300

    feat:ProjectsPage

--- src/Components/MyProjects.jsx
+++ src/Components/MyProjects.jsx
@@ -11,9 +11,19 @@ const MyProjects = () => {
     <div className={styles.Project} id="MyProjects">
         <h1>My Projects</h1>
         <div className={styles.line}></div>
-        <div className={styles.skew}>
+        <div className={styles.skew}></div>
 
+      <div className={styles.projCardWrapper}>
+        <div className={styles.projectCard}>
+          <p>Caso de estudo</p>
         </div>
+        <div className={styles.projectCard}>
+          <p>DOIS</p>
+        </div>
+        <div className={styles.projectCard}>
+          <p>Caso de estudo</p>
+        </div>
+      </div> 
     </div>
   )
 }
